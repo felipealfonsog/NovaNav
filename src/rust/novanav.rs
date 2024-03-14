5f3aee1dd2ce83cce3ee711734ed591c4a6fc186
Felipe <f.alfonso@res-ear.ch>
2024-03-14 11:15:45 -0300

    Fixes

--- src/rust/novanav.rs
+++ src/rust/novanav.rs
@@ -1,7 +1,6 @@
 use gtk::prelude::*;
 use gio::prelude::*;
-use std::cell::RefCell;
-use std::rc::Rc;
+use gdk::enums::key;
 use web_view::*;
 
 fn main() {
@@ -27,33 +26,34 @@ fn main() {
 
         // Conectar señales para los atajos de teclado
         let window_clone = window.clone();
-        window.connect_key_press_event(move |_, key| {
-            if key.get_keyval() == gdk::keys::constants::t && key.get_state().contains(gdk::ModifierType::CONTROL_MASK) {
-                let tab_label = gtk::Label::new("Nueva Pestaña");
-                let webview = Rc::new(RefCell::new(web_view::WebViewBuilder::new().title("Nueva Pestaña").build().unwrap()));
-                let tab_content = gtk::Box::new(gtk::Orientation::Vertical, 0);
-                tab_content.pack_start(&webview.borrow().gtk_widget().clone(), true, true, 0);
-                let close_button = gtk::Button::new_with_label("x");
-                let notebook_clone = notebook.clone();
-                close_button.connect_clicked(move |_| {
-                    if let Some(page_num) = notebook_clone.page_num(&tab_content) {
-                        notebook_clone.remove_page(Some(page_num));
-                    }
-                });
-                tab_content.pack_end(&close_button, false, true, 0);
-                notebook.append_page(&tab_content, Some(&tab_label));
-                webview.borrow().navigate("https://www.google.com").unwrap();
-                window_clone.show_all();
-                Inhibit(true)
-            } else if key.get_keyval() == gdk::keys::constants::minus && key.get_state().contains(gdk::ModifierType::CONTROL_MASK) {
-                if let Some(page_num) = notebook.get_current_page() {
-                    notebook.remove_page(Some(page_num));
+        window.connect_key_release_event(move |_, event| {
+            if let Some(key) = event.get_keyval() {
+                if key == key::t && event.get_state().contains(gdk::ModifierType::CONTROL_MASK) {
+                    let tab_label = gtk::Label::new(Some("Nueva Pestaña"));
+                    let webview = WebViewBuilder::new().title("Nueva Pestaña").build().unwrap();
+                    let tab_content = gtk::Box::new(gtk::Orientation::Vertical, 0);
+                    tab_content.pack_start(&webview.gtk_widget(), true, true, 0);
+                    let close_button = gtk::Button::with_label("x");
+                    let notebook_clone = notebook.clone();
+                    close_button.connect_clicked(move |_| {
+                        if let Some(page_num) = notebook_clone.page_num(&tab_content) {
+                            notebook_clone.remove_page(Some(page_num));
+                        }
+                    });
+                    tab_content.pack_end(&close_button, false, true, 0);
+                    notebook.append_page(&tab_content, Some(&tab_label));
+                    webview.navigate("https://www.google.com").unwrap();
                     window_clone.show_all();
+                    return Inhibit(true);
+                } else if key == key::minus && event.get_state().contains(gdk::ModifierType::CONTROL_MASK) {
+                    if let Some(page_num) = notebook.get_current_page() {
+                        notebook.remove_page(Some(page_num));
+                        window_clone.show_all();
+                    }
+                    return Inhibit(true);
                 }
-                Inhibit(true)
-            } else {
-                Inhibit(false)
             }
+            Inhibit(false)
         });
 
         // Mostrar todo
